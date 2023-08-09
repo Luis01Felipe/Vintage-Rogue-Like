0ff11e4d1483f206b118343774b03b5b9b07832c
Beater <104606257+Luis01Felipe@users.noreply.github.com>
2023-08-08 22:56:11 -0300

    base of the project #1

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,6 +5,8 @@ public class Program {
     public static void main(String[] args) {
         Player player = new Player("Luis", 50, 20, 20);
         Slime slime = new Slime("hugo", 50, 20, 20);
+        World matriz = new World(5, 5);
+        matriz.printMatriz();
         System.out.println(player.getName());
         System.out.println(slime.getName());
     }
---
+++ src/main/java/World.java
@@ -0,0 +1,19 @@
+public class World {
+    private int[][] matriz;
+    public World(int linhas, int colunas){
+        matriz = new int[linhas][colunas];
+    }
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public void printMatriz() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
